bb473696a0cd42444abad5caa14ea210a7d81c50
Manish Mishra <manish.mishra@knoldus.com>
2023-03-29 17:55:18 +0530

    Added Driver.scala

--- assignment-3/src/main/scala/com/knoldus/Driver.scala
+++ assignment-3/src/main/scala/com/knoldus/Driver.scala
@@ -4,6 +4,6 @@ object Driver extends App {
   val testObj = new FindNthNumberFromLast
   val list = List(1, 2, 3, 4, 5, 6)
   val nthElement = 2
-  val result = testObj.findNthElementFromLast(list, nthElement).getOrElse(None)
+  val result = testObj.findNthElementFromLast(list, nthElement).getOrElse(0)
   println(result)
 }
